e90a3e2e67de7e8031fec0874ad4fbec020d071c
moshkk <24ip232@git.scc>
2026-03-30 11:59:38 +0300

    SampleApp

--- SampleApp.API/Entities/User.cs
+++ SampleApp.API/Entities/User.cs
@@ -9,7 +9,7 @@ public class User : Base
 {
     public string Name { get; set; } = string.Empty;
     public required string Login { get; set; } = string.Empty;
-    public byte[] PasswordHash { get; set; }
-    public byte[] PasswordSalt { get; set; }
-    public string Token { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
+    public required string Token { get; set; }
 }
